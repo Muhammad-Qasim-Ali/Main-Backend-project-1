8268b5a701781a98538ccf00a64232d410bf4ad0
Muhammad-Qasim-Ali <muhqasimali50@gmail.com>
2024-11-30 19:50:53 +0500

    completed video 26 and part 4 of backend series

--- routes/ownersRouter.js
+++ routes/ownersRouter.js
@@ -1,8 +1,34 @@
 const express = require('express')
 const router = express.Router()
+const ownerModel = require("../models/owner-model")
+
+// its mean ager ham dvelopment phase ma hn tu /create py kam ho. othervise nhe.
+if(process.env.NODE_ENV === "development"){
+    router.post("/create", async function(req,res){
+        let owners = await ownerModel.find()
+        if(owners.length > 0) {
+            return res
+            .status(500)
+            .send("You don't have permission to create a new owner")
+        }
+
+        let {fullname, email, password} = req.body
+
+        let createdOwner = await ownerModel.create({
+            fullname,
+            email,
+            password,
+        })
+
+        res.status(201).send(createdOwner)
+    })
+}
 
 router.get("/", function(req,res){
     res.send("hey it's working")
 })
 
+
 module.exports = router
+
+// $env:NODE_ENV="development" --> write on terminal to set environment variable
