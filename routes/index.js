4bbb16863e5d03ae8b6824dc08219cf1676ec829
Muhammad-Qasim-Ali <muhqasimali50@gmail.com>
2024-12-07 12:16:22 +0500

    part 6 completed

--- routes/index.js
+++ routes/index.js
@@ -16,7 +16,11 @@ router.get("/shop", isLoggedIn, async function(req,res){
 })
 
 router.get("/cart", isLoggedIn, async function(req,res){
-    res.render("cart")
+    let user = await userModel.findOne({ email: req.user.email }).populate("cart")
+
+    const bill = (user.cart[0].price)+20-(user.cart[0].discount)
+
+    res.render("cart", { user, bill })
 })
 
 router.get("/addtocart/:productId", isLoggedIn, async function(req,res){
